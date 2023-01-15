041b37e41acba8599e751a24e95099f23108fc36
Harry K <harryk@harryk.dev>
2023-01-15 23:08:35 +0800

    updates

--- src/lib.rs
+++ src/lib.rs
@@ -5,20 +5,20 @@
 /////////////
 
 
-// -> Opting out of std doesn't change the size of your program. 
-// Rust programs only include the code from std that they use. So if you don't use std, like the programs on this page, 
-// you don't strictly speaking need to opt out of std to make small programs. 
+// -> Opting out of std doesn't change the size of your program.
+// Rust programs only include the code from std that they use. So if you don't use std, like the programs on this page,
+// you don't strictly speaking need to opt out of std to make small programs.
 // I choose to opt out because it means fewer surprises of the form "hey, my program suddenly grew by 12kiB, what gives?"
 
-//Let's declare a decent-sized image buffer in Rust. 
-//For simplicity, we'll put it at a fixed location in memory using static. 
+//Let's declare a decent-sized image buffer in Rust.
+//For simplicity, we'll put it at a fixed location in memory using static.
 //This isn't idiomatic Rust, but by opting out of std we have given up our ability to allocate memory
 
 const WIDTH: usize = 600;
 const HEIGHT: usize = 600;
 
 
-//let's add some global state to the WebAssembly module to keep track of the frame number. 
+//let's add some global state to the WebAssembly module to keep track of the frame number.
 use core::sync::atomic::{AtomicU32, Ordering};
 
 static FRAME: AtomicU32 = AtomicU32::new(0);
@@ -39,7 +39,7 @@ pub unsafe extern fn go() {
 // as possible.
 /*fn render_frame_safe(buffer: &mut [u32; WIDTH * HEIGHT]) {
     for pixel in buffer.iter_mut() {
-        // WebAssembly is little-endian, so the u32 contains 
+        // WebAssembly is little-endian, so the u32 contains
         // the pixel components in the reversed order 0xAA_BB_GG_RR.
         *pixel = 0xFF_FF_00_FF;
     }
@@ -54,12 +54,11 @@ pub unsafe extern fn go() {
     }
 }*/
 
-/**
- *Why? Well, WebAssembly currently doesn't have a sin operation
- *(though one has been proposed, and may be added in the future).
- *This means the Rust std library has to include its own implementation of sin, and 
- *a high-quality implementation of sin takes a fair amount of code.
- */
+
+ //Why? Well, WebAssembly currently doesn't have a sin operation
+ //(though one has been proposed, and may be added in the future).
+ //This means the Rust std library has to include its own implementation of sin, and
+ //a high-quality implementation of sin takes a fair amount of code.
 
 extern {
     fn js_sin(x: f32) -> f32;
@@ -71,14 +70,14 @@ fn sin(x: f32) -> f32 {
 
 fn render_frame_safe(buffer: &mut [u32; WIDTH * HEIGHT]) {
     // This line is new:
-    //  we want to update the BUFFER and then advance the FRAME. 
-    //  AtomicU32 provides a handy fetch_add operation that can retrieve 
+    //  we want to update the BUFFER and then advance the FRAME.
+    //  AtomicU32 provides a handy fetch_add operation that can retrieve
     //  the current frame number, and advance the global counter
     let f = FRAME.fetch_add(1, Ordering::Relaxed);
 
-    //A word of caution: calling sin for every pixel like I showed above takes a lot of processing power. 
-    //This is why I haven't embedded the WebAssembly program here as an example: 
-    //it would drain your battery while you're reading. 
+    //A word of caution: calling sin for every pixel like I showed above takes a lot of processing power.
+    //This is why I haven't embedded the WebAssembly program here as an example:
+    //it would drain your battery while you're reading.
     //Just like on computers of yore, call sin ahead of time and generate a lookup table!
      for y in 0..HEIGHT {
         for x in 0..WIDTH {
